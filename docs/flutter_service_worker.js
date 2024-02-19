7432162941c700dd342d12b71cf9f9de08f18040
kkb92 <koray.bilge@gmx.net>
2024-02-19 11:42:19 +0100

    update 002

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7bc66d1500ed1fcdb02e4f778ad58129",
-"/": "7bc66d1500ed1fcdb02e4f778ad58129",
+"index.html": "3b04d4273b5c9bbb5b85c66c033b02b5",
+"/": "3b04d4273b5c9bbb5b85c66c033b02b5",
 "main.dart.js": "da26124131d475a73e0e4843ba5e08b5",
 "manifest.json": "5516f0e9e96ebeaa4c08f3a2dbab3fee",
 "version.json": "a3a7612ab0c1ca8288f5a81606744495"};
